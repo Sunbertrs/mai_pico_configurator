6fc787b49e8d671602d971274275d8f2f3976b54
Sunbertrs <3127723014@qq.com>
2025-05-19 05:27:39 +0800

    fix: 主要是加入自定义定义的输入

--- cmds/gpio.py
+++ cmds/gpio.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 
 from communication import get_gpio_info, get_hid_mode, adjust_gpio_main_button, gpio_reset
 from draw import draw_title_and_prompting_keys, draw_selecting_options, resize_and_display
-from preset_var import cmds_gpio_text, CANVAS_FONT_SET, message_box_prompts, KEY_PROMPTING_POSITION, area_title_position, area_subtitl_position, GPIO_DEFAULT_DEFINITION, NKRO_KEY, CANVAS_CENTER_POSITION
+from preset_var import cmds_gpio_text, CANVAS_FONT_SET, message_box_prompts, area_title_position, area_subtitl_position, GPIO_DEFAULT_DEFINITION, NKRO_KEY
 
 def main(instance):
     global _inst
@@ -59,22 +59,47 @@ def display_main_button():
     for i, j in enumerate(title_display_position, start=1):
         draw.text(j, str(i), font=CANVAS_FONT_SET[0], fill="#000")
     _inst.root.bind("<KeyPress-BackSpace>", lambda _:selecting(0))
-    edit_main_button(None, 0, get_hid_mode(ignore_stuck=1)[-1])
+    edit_main_button("", 0, get_hid_mode(ignore_stuck=1)[-1])
 
 def edit_main_button(detected_value, current, key_position):
     global gpio_definition
-    if detected_value is not None:
-        gpio_definition[current-1] = GPIO_DEFAULT_DEFINITION[NKRO_KEY[int(key_position)-1].index(detected_value)]
+    if not detected_value.startswith("Insert"):
+        if detected_value == "Reset":
+            current = current - 1
+        else:
+            gpio_definition[current-1] = GPIO_DEFAULT_DEFINITION[NKRO_KEY[int(key_position)-1].index(detected_value)]
+    else:
+        gpio_definition[current-1] = detected_value.replace("Insert", "")
     image = prompt_image.copy()
     draw = ImageDraw.Draw(image)
+
+    if detected_value.startswith("Insert") and len(detected_value) < 10: current = current - 1
     for i, j in zip(subtitl_display_position, enumerate(gpio_definition)):
         draw.text(i, j[1], font=CANVAS_FONT_SET[1], fill="#E00" if current == j[0] else "#000")
+    if detected_value.startswith("Insert") and len(detected_value) < 10: current = current + 1
+
     for i in NKRO_KEY[int(key_position)-1]:
         _inst.root.bind(f'<KeyPress-{i}>', lambda a: edit_main_button(a.keysym, current+1, key_position))
+    _inst.root.bind('<KeyPress-Insert>', lambda a: edit_main_button("InsertGP", current+1, key_position))
+
+    if detected_value.startswith("Insert"):
+        _inst.root.unbind('<KeyPress-Insert>')
+        for i in NKRO_KEY[int(key_position)-1]:
+            _inst.root.unbind(f'<KeyPress-{i}>')
+        if len(detected_value) < 10:
+            for i in range(10):
+                _inst.root.bind(f'<KeyPress-{i}>', lambda a: edit_main_button(detected_value + a.keysym, current, key_position))
+        elif len(detected_value) == 10 and int(detected_value[-2:]) <= 28 and gpio_definition[current] != gpio_definition[current-1]:
+            for i in range(10):
+                _inst.root.unbind(f'<KeyPress-{i}>')
+            edit_main_button("Reset", current+1, key_position)
+        elif len(detected_value) >= 10 and int(detected_value[-2:]) > 28:
+            edit_main_button("InsertGP", current, key_position)
+
     if current == 8:
         confirm_main_button(image, key_position)
     else:
-        draw.text(KEY_PROMPTING_POSITION, cmds_gpio_text[5], fill="#000", font=CANVAS_FONT_SET[1])
+        draw_title_and_prompting_keys(draw, "", cmds_gpio_text[5])
     resize_and_display(_inst, image)
 
 def confirm_main_button(image, key_position):
